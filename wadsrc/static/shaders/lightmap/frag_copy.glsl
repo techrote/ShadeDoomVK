e62d74355e9cc7f71b67098267d337aabf0b4151
tom <261801702+techrote@users.noreply.github.com>
2026-09-19 19:17:31 +0100

    PF-012: implement bounded live probe-map selector

--- wadsrc/static/shaders/lightmap/frag_copy.glsl
+++ wadsrc/static/shaders/lightmap/frag_copy.glsl
@@ -1,93 +1,63 @@
 
 layout(set = 0, binding = 0) uniform sampler2D Tex;
 
+layout(push_constant) uniform PushConstants
+{
+	int SrcTexSize;
+	int DestTexSize;
+	int ProbeCount;
+	int Padding;
+};
+
+struct ProbeSelectionEntry
+{
+	vec3 position;
+	uint textureIndex;
+};
+
+layout(std430, set = 0, binding = 2) buffer readonly ProbeSelectionBuffer
+{
+	ProbeSelectionEntry probes[];
+};
+
 layout(location = 0) in vec2 TexCoord;
 layout(location = 1) in vec3 WorldPos;
 
 layout(location = 0) out vec4 FragColor;
 layout(location = 1) out uvec4 FragProbe;
 
-#if 1
-
-uint findClosestProbe(vec3 pos, float size)
+uint findClosestProbe(vec3 pos, float radius)
 {
-	return 0;
-}
+	const uint fallbackIndex = 0u;
+	const uint maxProbeMapIndex = 0xffffu;
+	float radiusSquared = radius * radius;
+	float closestDistanceSquared = radiusSquared;
+	uint closestTextureIndex = fallbackIndex;
+	bool found = false;
 
-#else
-
-struct ProbeNode
-{
-	vec2 center;
-	vec2 extents;
-	int left;
-	int right;
-	uint probeIndex;
-	int padding0;
-	vec3 probePos;
-	float padding1;
-};
-
-layout(std430, set = 0, binding = 1) buffer readonly ProbeBuffer
-{
-	int probeNodeRoot;
-	int probebufferPadding1;
-	int probebufferPadding2;
-	int probebufferPadding3;
-	ProbeNode probeNodes[];
-};
-
-bool isProbeNodeLeaf(int nodeIndex)
-{
-	return probeNodes[nodeIndex].probeIndex != 0;
-}
-
-bool overlapAABB(vec2 center, vec2 extents, int nodeIndex)
-{
-	vec2 d = center - probeNodes[nodeIndex].center;
-	vec2 p = extents + probeNodes[nodeIndex].extents - abs(d);
-	return p.x >= 0.0 && p.y >= 0.0;
-}
-
-uint findClosestProbe(vec3 pos, float size)
-{
-	float probeDistSqr = 0.0;
-	uint probeIndex = 0;
-	int stack[64];
-	int stackIndex = 0;
-	stack[stackIndex++] = probeNodeRoot;
-	do
+	for (int i = 0; i < ProbeCount; ++i)
 	{
-		int a = stack[--stackIndex];
-		if (overlapAABB(pos, vec2(size, size), a))
-		{
-			if (isProbeNodeLeaf(a))
-			{
-				vec3 probePos = probeNodes[a].probePos;
-				vec3 d = probePos - pos;
-				float distSqr = dot(d, d);
-				if (probeIndex == 0 || probeDistSqr > distSqr)
-				{
-					probeIndex = probeNodes[a].probeIndex;
-					probeDistSqr = distSqr;
-				}
-			}
-			else
-			{
-				stack[stackIndex++] = nodes[a].right;
-				stack[stackIndex++] = nodes[a].left;
-			}
-		}
-	} while (stackIndex > 0);
-	return probeIndex;
-}
+		ProbeSelectionEntry candidate = probes[i];
+		if (candidate.textureIndex == fallbackIndex || candidate.textureIndex > maxProbeMapIndex)
+			continue;
 
-#endif
+		vec3 delta = candidate.position - pos;
+		float distanceSquared = dot(delta, delta);
+		if (distanceSquared <= radiusSquared && (!found || distanceSquared < closestDistanceSquared))
+		{
+			closestDistanceSquared = distanceSquared;
+			closestTextureIndex = candidate.textureIndex;
+			found = true;
+		}
+	}
+
+	return closestTextureIndex;
+}
 
 void main()
 {
 	uint probeIndex = findClosestProbe(WorldPos, 512.0);
 
 	FragColor = texture(Tex, TexCoord);
-	FragProbe.x = probeIndex;
+	FragProbe = uvec4(probeIndex, 0u, 0u, 0u);
 }
